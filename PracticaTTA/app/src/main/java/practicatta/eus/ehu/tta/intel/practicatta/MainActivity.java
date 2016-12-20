80b0132c0fc5a35e4e79a2f13f05905caa1240d7
cristian <cristianllagunoiglesias@gmail.com>
2016-12-20 18:21:30 +0100

    Creación proyecto PracticaTTA

---
+++ PracticaTTA/app/src/main/java/practicatta/eus/ehu/tta/intel/practicatta/MainActivity.java
@@ -0,0 +1,13 @@
+package practicatta.eus.ehu.tta.intel.practicatta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
